baf5c4ca61f26c31d776cd6dfe892a1daa8d70fa
Limetop <alexandr246@vk.com>
2025-09-06 19:51:44 +0300

    Starting creating gui and translations

--- commands.py
+++ commands.py
@@ -1,24 +1,147 @@
-import time
+from time import sleep
+import tkinter as tk
+from webbrowser import open_new_tab
+import logging
+
+logging.basicConfig(level="INFO", format="[%(name)s] %(levelname)s -> %(message)s")
+logger = logging.getLogger("commands")
 
 
-def command_recognition(command, ser):
+def text_to_number(text):
+    numbers = {
+        "ноль": 0,
+        "один": 1,
+        "два": 2,
+        "три": 3,
+        "четыре": 4,
+        "пять": 5,
+        "шесть": 6,
+        "семь": 7,
+        "восемь": 8,
+        "девять": 9,
+        "десять": 10,
+        "одиннадцать": 11,
+        "двенадцать": 12,
+        "тринадцать": 13,
+        "четырнадцать": 14,
+        "пятнадцать": 15,
+        "шестнадцать": 16,
+        "семнадцать": 17,
+        "восемнадцать": 18,
+        "девятнадцать": 19,
+        "двадцать": 20,
+        "тридцать": 30,
+        "сорок": 40,
+        "пятьдесят": 50,
+        "шестьдесят": 60,
+        "семьдесят": 70,
+        "восемьдесят": 80,
+        "девяносто": 90,
+        "сто": 100
+    }
+
+    words = text.lower().split()
+    total = 0
+
+    for word in words:
+        if word in numbers:
+            total += numbers[word]
+
+    return total if total <= 100 else None
+
+
+def computer(command):
+    return "комп" in command or "ноут" in command
+
+
+def show_volume_change(current, new):
+    root = tk.Tk()
+    root.title("")
+    root.attributes('-topmost', True)
+    label = tk.Label(root, text=f"было: {int(current * 100)}, стало: {int(new * 100)}", font=("Arial", 16))
+    label.pack(pady=20)
+    root.after(3000, root.destroy)
+    root.mainloop()
+
+
+def command_recognition(command: str, volume):
+    if "громче" in command:
+        current = volume.GetMasterVolumeLevelScalar()
+        add = 0.05
+        new = min(current + add, 1.0)
+        volume.SetMasterVolumeLevelScalar(new, None)
+        print(f"было: {current}, стало {new}")
+        show_volume_change(current, new)
+    elif "тише" in command:
+        current = volume.GetMasterVolumeLevelScalar()
+        remove = 0.05
+        new = max(current - remove, 0.0)
+        volume.SetMasterVolumeLevelScalar(new, None)
+        print(f"было: {current}, стало {new}")
+        show_volume_change(current, new)
+    elif "громкость на" in command:
+        current = volume.GetMasterVolumeLevelScalar()
+        word = command[23:]
+        digit = text_to_number(word)
+        if digit is not None:
+            volume.SetMasterVolumeLevelScalar(digit / 100, None)
+            print(f"было: {current}, стало {digit / 100}")
+            show_volume_change(current, digit / 100)
+        else:
+            print("error: not a digit")
+    elif "звук на" in command:
+        current = volume.GetMasterVolumeLevelScalar()
+        word = command[18:]
+        digit = text_to_number(word)
+        if digit is not None:
+            volume.SetMasterVolumeLevelScalar(digit / 100, None)
+            print(f"было: {current}, стало {digit / 100}")
+            show_volume_change(current, digit / 100)
+        else:
+            print("error: not a digit")
+    elif "найди" in command:
+        q = command[16:]
+        open_new_tab(f"https://www.google.com/search?q={q}")
+
+
+def command_recognition_arduino(command, ser):
     if "вкл" in command:
-        print("вкл")
-        Commands.on(ser)
+        if "свет" in command:
+            print("вкл св")
+            ArduinoCommands.on_light(ser)
+        elif "звук" in command:
+            print("вкл зв")
+            ArduinoCommands.on_sound(ser)
     elif "выкл" in command:
-        print("выкл")
-        Commands.off(ser)
+        if "свет" in command:
+            print("выкл св")
+            ArduinoCommands.off_light(ser)
+        elif "звук" in command:
+            print("выкл зв")
+            ArduinoCommands.off_sound(ser)
 
 
-class Commands:
+class ArduinoCommands:
     @staticmethod
-    def on(ser):
-        time.sleep(3)
+    def on_light(ser):
+        sleep(3)
         ser.write(b'Hello world')
         ser.write(b'1')
 
     @staticmethod
-    def off(ser):
-        time.sleep(3)
+    def off_light(ser):
+        sleep(3)
         ser.write(b'Hello world')
         ser.write(b'0')
+
+    @staticmethod
+    def on_sound(ser):
+        sleep(3)
+        ser.write(b'Hello world')
+        ser.write(b'2')
+
+    @staticmethod
+    def off_sound(ser):
+        sleep(3)
+        ser.write(b'Hello world')
+        ser.write(b'3')
--- voice_recognition.py
+++ voice_recognition.py
@@ -1,9 +1,14 @@
 import json
+import logging
 import pyaudio
 from vosk import Model, KaldiRecognizer
-from commands import command_recognition
+from commands import command_recognition_arduino, command_recognition, computer
 from serial import Serial
 from serial.tools.list_ports import comports
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from comtypes import CLSCTX_ALL
+from keyboard import add_hotkey
+import gettext
 
 
 def get_com():
@@ -11,8 +16,18 @@ def get_com():
     return ports[0].device
 
 
+# logger
+logging.basicConfig(level="INFO", format="[%(name)s] %(levelname)s -> %(message)s")
+logger = logging.getLogger("voice_recognition")
+
+# locate
+t = gettext.translation(domain='messages', localedir='locale', languages=["ru", "en"])
+_ = t.gettext
+
+logger.info(_("Starting..."))
+
 # model
-model = Model("model")
+model = Model("model_small_USE_THIS")
 recognizer = KaldiRecognizer(model, 16000)
 
 # PyAudio
@@ -21,24 +36,58 @@ stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, fram
 stream.start_stream()
 
 # serial
-ser = Serial(get_com(), 9600)
 
-print("Начинаю распознавание речи...")
+while True:
+    q = input("Arduino? да/нет ")
+    if q == "да":
+        ser = Serial(get_com(), 9600)
+        arduino = True
+        break
+    elif q == "нет":
+        arduino = False
+        break
+
+# pycaw
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = interface.QueryInterface(IAudioEndpointVolume)
+
+# kb
+rec = True
+
+
+def l():
+    global rec
+    rec = not rec
+    logger.info(_("Stopped/started rec"))
+
+
+add_hotkey("win+z", l)
+
+logger.info(_("Started!"))
 
 try:
     while True:
+        if not rec:
+            continue
         data = stream.read(4000, exception_on_overflow=False)
         if recognizer.AcceptWaveform(data):
             result = recognizer.Result()
             res_text = json.loads(result)["text"]
-            command_recognition(res_text, ser)
+            if res_text:
+                logger.info(_(f"Recognized text: {res_text}"))
+                if computer(res_text):
+                    if arduino:
+                        command_recognition_arduino(res_text, ser)
+                    command_recognition(res_text, volume)
         else:
             partial_result = recognizer.PartialResult()
-            print(json.loads(partial_result)["partial"], end='\r')
 except KeyboardInterrupt:
-    print("Завершение распознавания речи...")
+    logger.info(_("Finishing..."))
 finally:
-    ser.close()
+    if arduino:
+        ser.close()
     stream.stop_stream()
     stream.close()
     p.terminate()
+    logger.info("Finished!")
